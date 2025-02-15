fec89c413111cb7dbd383b70aafd01471eba0a66
Ali <alimakhloufj@gmail.com>
2025-02-16 01:04:55 +0200

    Added rate limit middleware.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 const routes = require("./routes/heroRoutes");
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.use(cors());
+
+// Apply rate limiting middleware
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per window
+  message: "Too many requests from this IP, please try again later.",
+});
+
+app.use(limiter);
 
 app.use("/", routes);
 
